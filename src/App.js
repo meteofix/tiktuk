9bd9ffee85d81433e133c7f34b1a3556d05db460
Meteofix <meteofix@gmail.com>
2021-11-17 01:52:28 +0200

    initial project structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import {BrowserRouter, Link} from "react-router-dom";
+import AppRouter from "./components/AppRouter";
+import {TEST_API_ROUTE, TEST_CONTEXT_ROUTE, TRENDING_FEED_ROUTE} from "./utils/consts";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    return (
+      <BrowserRouter>
+          <div className="App">
+              <Link to={TRENDING_FEED_ROUTE}>TikTuk</Link>
+              <Link to={TEST_API_ROUTE}>TestAPI</Link>
+              <Link to={TEST_CONTEXT_ROUTE}>TestContext</Link>
+              <AppRouter/>
+          </div>
+      </BrowserRouter>
   );
 }
 
