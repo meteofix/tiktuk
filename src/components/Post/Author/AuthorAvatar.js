48325ad4e7f7f418904789d0a3de0df3274fdc4c
Meteofix <meteofix@gmail.com>
2021-11-17 02:03:07 +0200

    chore: add Author components for Post.js

---
+++ src/components/Post/Author/AuthorAvatar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+import classes from "./AuthorAvatar.module.css";
+
+const AuthorAvatar = ({authorLink, avatar, setIsHover}) => {
+    return (
+        <Link to={authorLink} className={classes.postAvatar}>
+                <span className={classes.tiktokAvatar}
+                      onMouseEnter={() => setIsHover(true)}
+                      onMouseLeave={() => setIsHover(false)}
+                >
+                    <img alt='' src={avatar}/>
+                </span>
+        </Link>
+    );
+};
+
+export default AuthorAvatar;
---
+++ src/components/Post/Author/AuthorInfo.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import classes from './AuthorInfo.module.css'
+import {Link} from "react-router-dom";
+import UserVerifiedIcon from "../../../UI/icons/UserVerifiedIcon";
+
+const AuthorInfo = ({authorLink, isHover, setIsHover, authorMeta}) => {
+    return (
+        <div className={classes.authorInfo}>
+            <Link to={authorLink}
+                  onMouseEnter={() => setIsHover(true)}
+                  onMouseLeave={() => setIsHover(false)}>
+                <h3 className={
+                    isHover ?
+                        `${classes.authorId} ${classes.underline}`
+                        :
+                        classes.authorId
+                }
+                >
+                    {authorMeta.name}
+                    {authorMeta.verified ?
+                        <UserVerifiedIcon cl={classes.verified}/>
+                        : ''
+                    }
+                </h3>
+            </Link>
+            <Link to={authorLink}
+                  onMouseEnter={() => setIsHover(true)}
+                  onMouseLeave={() => setIsHover(false)}
+            >
+                <h4 className={classes.authorNickName}>{authorMeta.nickName}</h4>
+            </Link>
+        </div>
+    );
+};
+
+export default AuthorInfo;
